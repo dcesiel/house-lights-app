56d62039805806c221e82b801bee4d9226bfbc23
David Cesiel <dcesiel@gmail.com>
2019-01-20 15:32:03 -0600

    Adding redux

--- src/Home.js
+++ src/Home.js
@@ -7,9 +7,9 @@ export const Home = ({ homeName = "Home" }) => (
     <div className={'home-container'}>
         <div className={'title'}>{homeName}</div>
         <LightSwitch on={false} containerCssClass={'master-switch'}/>
-        <Room lightOn={false} roomName={'Room 1'} containerCssClass={"room-1"}/>
-        <Room lightOn={true} roomName={'Room 2'} containerCssClass={"room-2"}/>
-        <Room lightOn={true} roomName={'Room 3'} containerCssClass={"room-3"}/>
+        <Room lightOn={false} roomName={'Kitchen'} containerCssClass={"room-1"}/>
+        <Room lightOn={true} roomName={'Living Room'} containerCssClass={"room-2"}/>
+        <Room lightOn={true} roomName={'Bedroom'} containerCssClass={"room-3"}/>
         <LightSwitch on={false} containerCssClass={'room-1-switch'}/>
         <LightSwitch on={true} containerCssClass={'room-2-switch'}/>
         <LightSwitch on={true} containerCssClass={'room-3-switch'}/>
---
+++ src/reducers/home.js
@@ -0,0 +1,6 @@
+export const home = (state, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
