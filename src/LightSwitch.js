2f1514c1d64e6e00bb2f8db72653cb7045e6c9a0
David Cesiel <dcesiel@gmail.com>
2019-02-13 00:28:37 -0600

    Adding an updated lightswitch with proper toggling

--- src/LightSwitch.js
+++ src/LightSwitch.js
@@ -2,15 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './LightSwitch.css'
 
-export const LightSwitch = ({ on = false, containerCssClass, handleToggle }) => (
-    <div className={containerCssClass}>
-        <label class="switch">
-            <input class="switch-input" type="checkbox" checked={on} onChange={handleToggle}/>
-            <span class="switch-label" data-on="On" data-off="Off"></span>
-            <span class="switch-handle"></span>
-        </label>
-    </div>
-);
+export const LightSwitch = ({ on = false, containerCssClass, handleToggle }) => {
+    console.log(handleToggle)
+    return (
+        <div className={containerCssClass}>
+            <label class="switch">
+                <input class="switch-input" type="checkbox" checked={on} onChange={event => handleToggle(event.target.checked)}/>
+                <span class="switch-label" data-on="On" data-off="Off"></span>
+                <span class="switch-handle"></span>
+            </label>
+        </div>
+    );
+}
 
 LightSwitch.propTypes = {
     on: PropTypes.bool,
